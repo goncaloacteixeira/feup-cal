3b25e45bcaa291038e6dab9a9825715272401661
Gonçalo Teixeira <skidru00@gmail.com>
2020-03-02 17:20:53 +0000

    added Labirinth solved

--- cal_fp02_CLion/Tests/Labirinth.cpp
+++ cal_fp02_CLion/Tests/Labirinth.cpp
@@ -9,39 +9,51 @@ using namespace std;
 
 
 
-Labirinth::Labirinth(int values[10][10])
-{
-	for (int i = 0; i < 10; i++)
-		for (int j = 0; j < 10; j++)
-			labirinth[i][j] = values[i][j];
+Labirinth::Labirinth(int values[10][10]) {
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            labirinth[i][j] = values[i][j];
+
+    this->initializeVisited();
 }
 
 
-void Labirinth::initializeVisited()
-{
-	for (int i = 0; i < 10; i++)
-		for (int j = 0; j < 10; j++)
-			visited[i][j] = false;
+void Labirinth::initializeVisited() {
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            visited[i][j] = false;
 }
 
 
 
 
-void  Labirinth::printLabirinth()
-{
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-			cout << labirinth[i][j] << " ";
+void  Labirinth::printLabirinth() {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++)
+            cout << labirinth[i][j] << " ";
 
-		cout << endl;
-	}
+        cout << endl;
+    }
+    cout << endl;
 }
 
 
-bool Labirinth::findGoal(int x, int y)
-{
-	return false;
+bool Labirinth::findGoal(int x, int y) {
+    if (labirinth[x][y] == 2) {
+        return true;
+    }
+    if (labirinth[x][y] == 0) {
+        return false;
+    }
+    else {
+        if (this->findGoal(x+1,y)) {
+            return true;
+        }
+        if (this->findGoal(x, y+1)) {
+            return true;
+        }
+        return false;
+    }
 }
 
 
